621bb18e61e65f9891db9d3262a2093eff4db062
nina1012 <labferment10@gmail.com>
2020-04-26 15:24:04 +0200

    Updates

---
+++ precache-manifest.3b30a5236a95c67f277be368cdca522d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798e63ad7055cdaa1d6de9f39d0e457a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5e9856f1c72bcf854f2",
+    "url": "/monsters-rolodex/static/css/main.5a991a3f.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e9856f1c72bcf854f2",
+    "url": "/monsters-rolodex/static/js/main.c37b7c1e.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
